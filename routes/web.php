eb7891a56756846362cfebab7873b4d687eccc8d
saiwolf <saiwolf@swmnu.net>
2017-04-29 01:25:42 +0000

    Finished EP. 6 DB Builder

--- routes/web.php
+++ routes/web.php
@@ -11,11 +11,16 @@
 |
 */
 
-Route::get('/', function () {
-    $tasks = [
-        'Go to the store',
-        'Finish watching the screencast',
-        'Do something else'
-        ];
-    return view('welcome', compact('tasks'));
+Route::get('/tasks', function () {
+    
+    $tasks = DB::table('tasks')->get();
+    
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+    
+    $task = DB::table('tasks')->find($id);
+    
+    return view('tasks.show', compact('task'));
 });
