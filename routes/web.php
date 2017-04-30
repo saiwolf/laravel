235d893f017cfdc5421430c4f858e5a77af50df4
saiwolf <saiwolf@swmnu.net>
2017-04-30 18:19:02 +0000

    Before Artisan migrate reset

--- routes/web.php
+++ routes/web.php
@@ -11,16 +11,18 @@
 |
 */
 
+use App\Task;
+
 Route::get('/tasks', function () {
     
-    $tasks = DB::table('tasks')->get();
+    $tasks = Task::all();
     
     return view('tasks.index', compact('tasks'));
 });
 
 Route::get('/tasks/{task}', function ($id) {
     
-    $task = DB::table('tasks')->find($id);
+    $task = Task::find($id);
     
     return view('tasks.show', compact('task'));
 });
