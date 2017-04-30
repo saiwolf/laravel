fad980718aa37cc734736de01d9f9409a6deb5da
saiwolf <saiwolf@swmnu.net>
2017-04-30 18:37:36 +0000

    Finished Ep. 7

--- app/Task.php
+++ app/Task.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    
-   
+    public function scopeIncomplete($query)
+    {
+        return $query->where('completed', 0);
+    }
 }
