427832280ad56f7457d65dc34e5c0a696b8118db
saiwolf <saiwolf@swmnu.net>
2017-04-30 18:50:06 +0000

    Finished Episode 8

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,6 @@
 |
 */
 
-use App\Task;
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
 
-Route::get('/tasks', function () {
-    
-    $tasks = Task::all();
-    
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    
-    $task = Task::find($id);
-    
-    return view('tasks.show', compact('task'));
-});
