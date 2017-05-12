36c64dce83665cc3c615bd5e4329a6eb6f7e7414
saiwolf <saiwolf@swmnu.net>
2017-05-12 21:06:00 +0000

    Fixed up the date subheading

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -1,6 +1,6 @@
 <div class="blog-post">
     <h2 class="blog-post-title">{{ $post->title }}</h2>
-        <p class="blog-post-meta">{{ $post->created_at->format('F jS , Y \a\t g:i a') }}</p>
-        {{ $post->body }}
+    <p class="blog-post-meta">{{ $post->created_at->format('F jS , Y \@ g:i A') }}</p>
+    {{ $post->body }}
 </div>
 <!-- /.blog-post -->
